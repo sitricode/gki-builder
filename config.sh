5201e3d68bc7e80eae49b499150b07a56bfa3e08
sitricode <sitricode@outlook.co.id>
2025-06-17 19:43:12 +0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -18,7 +18,7 @@ ANYKERNEL_BRANCH="gki"
 
 # Kernel
 KERNEL_REPO="https://github.com/sitricode/gki_5.10"
-KERNEL_BRANCH="android12-5.10"
+KERNEL_BRANCH="master"
 KERNEL_DEFCONFIG="gki_defconfig"
 DEFCONFIG_FILE="$workdir/common/arch/arm64/configs/$KERNEL_DEFCONFIG"
 
