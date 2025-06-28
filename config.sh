295fe90316aedcc5b2fdc3e3981623c475265c58
sitricode <sitricode@outlook.co.id>
2025-06-28 09:31:53 +0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -42,7 +42,7 @@ USE_CUSTOM_CLANG="false"
 CUSTOM_CLANG_SOURCE="https://github.com/liliumproject/clang/releases/download/20250609/lilium_clang-20250609.tar.gz"
 CUSTOM_CLANG_BRANCH=""
 #Clang setting
-USE_THIN_LTO="false"
+USE_THIN_LTO="true"
 OPT_CC_PATCH="falsew"
 
 # Zip name
