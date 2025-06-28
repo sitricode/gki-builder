dfe4392602d5682c1e41aafad98a86ec7052dc22
sitricode <sitricode@outlook.co.id>
2025-06-28 09:36:33 +0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -8,8 +8,8 @@ GKI_VERSION="android12-5.10"
 
 # Build variables
 export TZ="Asia/Jakarta"
-export KBUILD_BUILD_USER="sitricode"
-export KBUILD_BUILD_HOST="github"
+export KBUILD_BUILD_USER="sitri"
+export KBUILD_BUILD_HOST="build-host"
 export KBUILD_BUILD_TIMESTAMP=$(date)
 
 # AnyKernel variables
