afaa8431a94620623665bf203ee379ad65d9023b
sitricode <sitricode@outlook.co.id>
2025-06-29 21:19:04 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -540,8 +540,9 @@ fi
 
 if [[ $STATUS == "BETA" ]]; then
     #Send all files in the artifacts directory
-    for file in "$workdir/artifacts"/*; do
+    for file in $workdir/artifacts/*; do
         if [[ -f "$file" ]]; then
+            echo $file
             reply_file "$MESSAGE_ID" "$file"
         fi
     done
